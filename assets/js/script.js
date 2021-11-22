24834d3e00d3d57dfaf789f5c4b410cf7f2c363f
Nicholas Nolen <nicnolen@ymail.com>
2021-11-22 16:28:09 -0600

    captured form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,25 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 // FUNCTIONS
 // Dynamically create the task item
 var createTaskHandler = function(event) {
-    
-    // Preventing the browser from using its default behavior
+    // preventing the browser from using its default behavior
     event.preventDefault();
+    
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    // create new task item
+    // create new list item
     var listItemEl = document.createElement("li");
-    // style
     listItemEl.className = "task-item";
-    // add text
-    listItemEl.textContent = "This is a new task.";
+    
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // giv div a class name
+    taskInfoEl.className = "task-info";
+    
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
     // append (add) the child element (li) to the end of the parent element (ul)
     tasksToDoEl.appendChild(listItemEl);
 }
