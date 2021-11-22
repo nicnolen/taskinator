26aef6c21f5fab2414a86f2751c59745f27bb110
Nicholas Nolen <nicnolen@ymail.com>
2021-11-22 15:41:57 -0600

    allowed submit button and enter key to submit the form and removed browser default behavior

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 // GLOBAL VARIABLES
-// Assign button element object representation to a variable. Not: (El identifies DOM elements)
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+// Assign form element object representation to a variable. Not: (El identifies DOM elements)
+var formEl = document.querySelector("#task-form");
 // Assign task list element (li) object representation to a variable
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // FUNCTIONS
 // Dynamically create the task item
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    
+    // Preventing the browser from using its default behavior
+    event.preventDefault();
+
     // create new task item
     var listItemEl = document.createElement("li");
     // style
@@ -19,4 +22,4 @@ var createTaskHandler = function() {
 }
 
 // Event Listener for list item DOM
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
